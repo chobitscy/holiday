dd3a99f21e94aeacfcd27a65cf0a9c7562e892a2
TouwaErioer <duhongyu627@gmail.com>
2021-03-13 14:34:06 +0800

    feat:build dest

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-    publicPath: './',
-    devServer: {
-        host: '0.0.0.0',
-        proxy: {
-            "/api": {
-                target: 'http://104.168.14.87:5000',
-                changeOrigin: true,
-                pathRewrite: {
-                    "^/api": ''
-                }
-            }
-        },
-    }
+    publicPath: './'
 };
